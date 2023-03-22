46f75fdfeb5879fc04492cc1cbdc2d4e8a8642e1
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2023-03-22 16:28:46 +0100

    Added more (generic)zsh aliases

--- dot_config/omz_custom/alias.zsh
+++ dot_config/omz_custom/alias.zsh
@@ -1,1 +1,12 @@
+alias calias="less ~/.config/omz_custom/alias.zsh"
+
 alias m="make"
+#Git
+alias g="git"
+alias gs="git status"
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias gdf="git diff"
+alias gac="git add . && dit commit"
+alias gam="git add . && git commit -m "
+alias push="git push"
+alias pull="git pull --rebase"
